dd00ecf1c723e5421f6fd47347a009d709363245
ansiaz <anvar.si@yandex.ru>
2019-03-17 21:46:48 +0500

    The first lab of BDD

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+          
+        }
+        public static int Partition(int[] array, int low, int high)
+        {
+            int marker = low;
+            for (int i = low; i <= high; i++)
+            {
+                if (array[i] <= array[high])
+                {
+                    int temp = array[marker];
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            return marker - 1;
+        }
+        public static void QuickSort(int[] array, int low, int high)
+        {
+            //rnd = new Random();
+            //rnd.Next(); // возвращает случайное число от 0 до int.MaxValue
+            //rnd.Next(0, 5); // возвращает случайное число от 0 до 4 включительно
+            if (low >= high)
+            {
+                return;
+            }
+            int pivot = Partition(array, low, high);
+            QuickSort(array, low, pivot - 1);
+            QuickSort(array, pivot + 1, high);
+        }
+    }
+}
---
+++ QuickSort/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTreeElenets()
+        {
+            int[] array = new int[] { 3, 2, 1 };
+            QuickSort.Program.QuickSort(array, 0, 2);
+            Assert.IsTrue(array[0] <= array[1] && array[1] <= array[2]);
+        }
+        [TestMethod]
+        public void TestOneHangredElements()
+        {
+            int[] array = new int[100];
+            for(int i = 0; i < array.Length;i++ )
+            {
+                array[i] = 1;
+            }
+            QuickSort.Program.QuickSort(array, 0, 99);
+        }
+
+        [TestMethod]
+        public void Test1000Element()
+        {
+            int[] array = new int[1000];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next();
+            }
+            QuickSort.Program.QuickSort(array, 0, 999);
+            for (int i = 0; i < 10; i++)
+            {
+                var x = rnd.Next(0,array.Length - 1);
+                var y = rnd.Next(0,array.Length - 1);
+                if (x > y)
+                {
+                    Assert.IsTrue(array[x] > array[y]);
+                }
+                else
+                if (x < y)
+                {
+                    Assert.IsTrue(array[x] < array[y]);
+                }
+                else
+                {
+                    Assert.IsTrue(array[x] == array[y]);
+                }
+            }
+            
+        }
+
+        [TestMethod]
+        public void TestEmptyElements()
+        {
+            int[] array = new int[] { };
+            QuickSort.Program.QuickSort(array, 0, 0);
+        }
+        [TestMethod]
+        public void TestMuchElements()
+        {
+            var rnd = new Random();
+            int[] array = new int[150000000];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0,array.Length - 1);
+            }
+            QuickSort.Program.QuickSort(array, 0, 149999999);
+        }
+    }
+}
